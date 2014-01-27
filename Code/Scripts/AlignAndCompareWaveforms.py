782e889259eda5f20cd701868e0f914714e57c68
Mike Boyle <boyle@astro.cornell.edu>
2014-01-27 16:33:33 -0500

    Add more description of what is going on

--- Code/Scripts/AlignAndCompareWaveforms.py
+++ Code/Scripts/AlignAndCompareWaveforms.py
@@ -2,6 +2,7 @@
 # compare.  The first one will be used as the fiducial waveform, and
 # everything else will be shifted to best agree with it.
 Files = ['/Users/boyle/Research/Data/SimulationAnnex/Catalog/SKS/d15_q1_sA_0_0_0.97_sB_0_0_0.97_ecc6e-4/Lev6/rhOverM_Asymptotic_GeometricUnits.h5/Extrapolated_N2.dir',
+         '/Users/boyle/Research/Data/SimulationAnnex/Catalog/SKS/d15_q1_sA_0_0_0.97_sB_0_0_0.97_ecc6e-4/Lev6/rhOverM_Asymptotic_GeometricUnits.h5/Extrapolated_N3.dir',
          '/Users/boyle/Research/Data/SimulationAnnex/Catalog/SKS/d15_q1_sA_0_0_0.97_sB_0_0_0.97_ecc6e-4/Lev6/rhOverM_Asymptotic_GeometricUnits.h5/Extrapolated_N4.dir']
 
 # This is an automatic legend generator for each of the lines in the
@@ -33,56 +34,63 @@ import matplotlib.pyplot as plt
 import GWFrames
 
 # Read in the data files
+print("Reading waveforms from data files...")
 Ws = [None]*len(Files)
 for i,File in enumerate(Files):
-    print("Reading '{0}'".format(File))
+    print("\tReading '{0}'".format(File))
     Ws[i] = GWFrames.ReadFromNRAR(File)
 print("Finished!\n")
-W0 = Ws[0]
+W_0 = Ws[0]
 
 # Transform each Waveform into its corotating frame
 print("Transforming to corotating frames...")
-for W in Ws:
+for i,W in enumerate(Ws):
+    print("\tTransforming {0} of {1}...".format(i+1, len(Ws)))
     W.TransformToCorotatingFrame()
     W.AlignDecompositionFrameToModes(tmid)
+print("Finished!\n")
+
+# Align to original Waveform
+print("Aligning all following waveforms to first...")
 xHat = GWFrames.Quaternion(0,1,0,0)
-R0_mid = GWFrames.Squad(W0.Frame(), W0.T(), [tmid])[0]
-for W in Ws[1:]:
+R0_mid = GWFrames.Squad(W_0.Frame(), W_0.T(), [tmid])[0]
+for i,W in enumerate(Ws[1:]):
+    print("\tAligning {0} of {1}...".format(i+1, len(Ws[1:])))
     # Rotate by pi if necessary
     Ri_mid = GWFrames.Squad(W.Frame(), W.T(), [tmid])[0]
     if((R0_mid*xHat*R0_mid.inverse()).dot(Ri_mid*xHat*Ri_mid.inverse())<0) :
         W.RotateDecompositionBasis(GWFrames.exp(GWFrames.Quaternion(0,0,0,pi/2)))
     # Align corotating frame of `W[1:]` to corotating frame of `W[0]`
-    W.AlignTimeAndFrame(W0, t1, t2);
+    W.AlignTimeAndFrame(W_0, t1, t2);
 print("Finished!\n")
 
 # First, we interpolate everything onto the common subset of time steps:
+print("Plotting differences...")
 Tini = max([W.T(0) for W in Ws])
 Tfin = min([W.T(W.NTimes()-1) for W in Ws])
-R0 = W0.Frame()[(W0.T()>=Tini) & (W0.T()<=Tfin)]
-T0 = W0.T()[(W0.T()>=Tini) & (W0.T()<=Tfin)]
-Abs0 = W0.Interpolate(T0).Abs(W0.FindModeIndex(2,2))
-Imid = argmin(abs(T0-tmid))
+R_0 = W_0.Frame()[(W_0.T()>=Tini) & (W_0.T()<=Tfin)]
+T_0 = W_0.T()[(W_0.T()>=Tini) & (W_0.T()<=Tfin)]
+Abs_0 = W_0.Interpolate(T_0).Abs(W_0.FindModeIndex(2,2))
+Imid = argmin(abs(T_0-tmid))
 plt.figure(0)
 plt.figure(1)
 for i,W in enumerate(Ws[1:]):
-    print("Plotting difference {0} of {1}...".format(i, len(Ws[1:])))
-    Ti = W.T()[(W.T()>=Tini) & (W.T()<=Tfin)]
-    Ri = GWFrames.Squad(W.Frame(), W.T(), T0)
-    RDelta = GWFrames.UnflipRotors(GWFrames.RDelta(R0, Ri, Imid))
+    print("\tPlotting {0} of {1}...".format(i+1, len(Ws[1:])))
+    R_i = GWFrames.Squad(W.Frame(), W.T(), T_0)
+    RDelta = GWFrames.UnflipRotors(GWFrames.RDelta(R_0, R_i, Imid))
     PhiDelta = array(GWFrames.angle(RDelta))
     plt.figure(0)
-    plt.semilogy(T0, 2.0*PhiDelta, label=Legends[i])
+    plt.semilogy(T_0, 2.0*PhiDelta, label=Legends[i])
     plt.figure(1)
-    Abs_i = W.Interpolate(T0).Abs(W.FindModeIndex(2,2))
-    plt.semilogy(T0, abs(Abs0-Abs_i)/Abs0, label=Legends[i])
+    Abs_i = W.Interpolate(T_0).Abs(W.FindModeIndex(2,2))
+    plt.semilogy(T_0, abs(Abs_0-Abs_i)/Abs_0, label=Legends[i])
 print("Finished!\n")
 
 plt.figure(0)
 plt.xlim((tmin,tmax))
 plt.ylim((1e-6,1))
 plt.ylabel(r'$2\Phi_\Delta$')
-# ylabel(r'$\Phi_{\Delta}\ =\ 2\, \left| \log \left( R_{1}\, \bar{R}_{2} \right) \right|$')
+# ylabel(r'$2\Phi_{\Delta}\ =\ 4\, \left| \log \left( R_{1}\, \bar{R}_{2} \right) \right|$')
 plt.xlabel(r'$(t-r_\ast)/M$')
 plt.title('Phase difference between waveforms')
 plt.legend(loc='upper left')
